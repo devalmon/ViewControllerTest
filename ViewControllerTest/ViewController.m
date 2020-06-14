d9452c0d987014d6c1ad6f29971adaf8ec6fc24c
Alexey B <phlood@yandex.ru>
2020-06-14 21:48:32 +0300

    roll out

--- ViewControllerTest/ViewController.m
+++ ViewControllerTest/ViewController.m
@@ -14,10 +14,49 @@
 
 @implementation ViewController
 
+- (void)loadView {
+    [super loadView];
+    NSLog(@"loadView");
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSLog(@"viewDidLoad");
+    
+    NSLog(@"%@\n%@\n%@", [[UIDevice currentDevice]name], [[UIDevice currentDevice]model], [[UIDevice currentDevice]systemVersion]);
 }
 
+// Called just before the view controller's view's layoutSubviews method is invoked. Subclasses can implement as necessary. The default is a nop.
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"viewWillAppear");
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSLog(@"viewDidAppear");
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    NSLog(@"viewWillLayoutSubviews");
+    
+}
+// Called just after the view controller's view's layoutSubviews method is invoked. Subclasses can implement as necessary. The default is a nop.
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    NSLog(@"viewDidLayoutSubviews");
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    NSLog(@"%f\n%f\n%@", size.width, size.height, coordinator);
+}
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
 
 @end
